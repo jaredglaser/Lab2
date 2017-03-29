5c471898dcb642e7aaa0b5575daf01d68513c3d0
jared <jared@LAPTOP-SR55OSKM>
2017-03-29 13:48:07 -0400

    modified Deck

--- PokerGame/src/main/java/pkgPokerBLL/Deck.java
+++ PokerGame/src/main/java/pkgPokerBLL/Deck.java
@@ -11,31 +11,31 @@ public class Deck {
 
 	private UUID DeckID;
 	private ArrayList<Card> DeckCards = new ArrayList<Card>();
-	
+
 	public Deck()
-    {
-        for (eSuit Suit : eSuit.values())
-        {
-            for (eRank Rank : eRank.values()) {
-            Card car = new Card(Rank,Suit);
-            DeckCards.add(car);
-        }
-        }
-        Collections.shuffle(DeckCards);
-    }
-   
-    public Card DrawCard()
-    {
-    	if(CardsToDraw() > 0){ //if there are cards left, draw one.
-    		return DeckCards.remove(0);
-    	}  
-    	return null; //TODO: Add an exception to handle this. ie: no cards left exception.
-    }
-    
-    public int CardsToDraw(){
-    	int numCards =  DeckCards.size()-1;
-    	System.out.println("There are " + numCards + " Cards Left in the Deck.");
-    	return numCards;
-    }
+	{
+		for (eSuit Suit : eSuit.values())
+		{
+			for (eRank Rank : eRank.values()) {
+				Card car = new Card(Rank,Suit);
+				DeckCards.add(car);
+			}
+		}
+		Collections.shuffle(DeckCards);
+	}
+
+	public Card DrawCard()
+	{
+		if(CardsToDraw() > 0){ //if there are cards left, draw one.
+			return DeckCards.remove(0);
+		}  
+		return null; //TODO: Add an exception to handle this. ie: no cards left exception.
+	}
+
+	public int CardsToDraw(){
+		int numCards =  DeckCards.size()-1;
+		System.out.println("There are " + numCards + " Cards Left in the Deck.");
+		return numCards;
+	}
 }
 
